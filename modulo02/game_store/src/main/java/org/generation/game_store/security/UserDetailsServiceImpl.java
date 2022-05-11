eb7e83f8b08591feac1a51396af40280ba611b99
Babbi Oliveira <oliveirababbi@gmail.com>
2022-05-11 12:31:30 -0300

    gamestore up

---
+++ modulo02/game_store/src/main/java/org/generation/game_store/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package org.generation.game_store.security;
+
+import java.util.Optional;
+import org.generation.game_store.model.Usuario;
+import org.generation.game_store.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		Optional<Usuario> usuario = userRepository.findByUsuario(userName);
+		usuario.orElseThrow(() -> new UsernameNotFoundException(userName + " not found."));
+		return usuario.map(UserDetailsImpl::new).get();
+	}
+}
